1e72a85670776bafe7b6dd401a20653f045adc69
Valdano Simão <valdanosimao@gmail.com>
2022-10-16 13:52:13 -0300

    criando atributos e o construtor da classe Email

---
+++ src/EmailApp/Email.java
@@ -0,0 +1,33 @@
+package EmailApp;
+
+import java.util.Scanner;
+
+public class Email {
+
+    /**
+     * Classe Scanner
+     */
+    Scanner entrada = new Scanner(System.in);
+
+    /**
+     * Atributos
+     */
+    private String primeiroNome;
+    private String segundoNome;
+    private String departamento;
+    private String email;
+    private String senha;
+    private Integer capacidadeEmail = 500;
+    private String emailAlternativo;
+
+    /**
+     * Construtor que recebe o primeiro e o segundo nome
+     */
+    public Email(String primeiroNome, String segundoNome) {
+        this.primeiroNome = primeiroNome;
+        this.segundoNome = segundoNome;
+        System.out.println("Novo Funcionário: " + this.primeiroNome + " " + this.segundoNome);
+    }
+
+
+}
